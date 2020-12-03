c52558d4bd69c46b1a7c6f81c9512676a5ac774c
Jose Maria Perez <jm.perez@globant.com>
2020-12-03 13:54:06 -0300

    ejercicios modificados

---
+++ RepositorioCoorporativo/01-JS-DOM-APIs/ejercicio04/archivo.js
@@ -0,0 +1,15 @@
+function showAlert() {
+    let x = document.getElementsByClassName("titulo");
+ 
+    fetch("https://api.github.com/search/repositories", {
+
+ 
+})
+
+.then(response => response.json())
+
+.then(data => {
+
+})
+
+} 
