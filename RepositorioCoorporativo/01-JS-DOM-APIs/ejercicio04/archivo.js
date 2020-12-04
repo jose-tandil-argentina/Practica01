a0ddf4e4f3f60953dc76de47558cc1eb9fc5cf11
Jose Maria Perez <jm.perez@globant.com>
2020-12-04 10:19:06 -0300

    ejercicios modificados

--- RepositorioCoorporativo/01-JS-DOM-APIs/ejercicio04/archivo.js
+++ RepositorioCoorporativo/01-JS-DOM-APIs/ejercicio04/archivo.js
@@ -1,15 +1,32 @@
 function showAlert() {
     let x = document.getElementsByClassName("titulo");
-    let type = "text" ;
-    fetch("https://api.github.com/search/repositories", {
+   // let type = "text";
+   
+   
+   
+   
+   
+    fetch("https://api.github.com/search/repositories?q=javascript&dni=16068555", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "dasdsad":"sadasd",
+        },
 
- 
-})
+    
 
-.then(response => response.json())
+    })
 
-.then(data => (x[0].innerHTML = data.documentation_url)
-
-)
-
+    .then(response => response.json())
+    
+            .then(data => {
+    
+                //debugger
+                (x[0].innerHTML = data.documentation_url)
+    
+    
+    
+            })
+    
+            
 } 
