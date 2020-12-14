6dc06505868fdd8d28c718edb067e942df9fd6ad
Jose Maria Perez <jm.perez@globant.com>
2020-12-14 16:02:32 -0300

    ejercicios modificados

--- ejercicioPractica06/practica06.js
+++ ejercicioPractica06/practica06.js
@@ -1,8 +1,8 @@
 
 function compararNum(){
   /* no toma el getElement*/
-  let num1 = parseInt(document.getElementsByClassName("num1").value)[0];
-  let num2 = parseInt(document.getElementsByClassName("num2").value)[0];
+  let num1 = parseInt(document.getElementsByClassName("num1").value);
+  let num2 = parseInt(document.getElementsByClassName("num2").value);
 
   if (num1 && num2) {
     if (num1 >= num2) {
