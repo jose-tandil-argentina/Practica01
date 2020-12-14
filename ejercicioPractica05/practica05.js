ef73752a984a3552878a3cbaf7ebc8fa0cc17b26
Jose Maria Perez <jm.perez@globant.com>
2020-12-14 16:01:54 -0300

    ejercicios modificados

--- ejercicioPractica05/practica05.js
+++ ejercicioPractica05/practica05.js
@@ -1,7 +1,7 @@
 function aniadirTexto() {
 /* no toma el getElement*/
-  let texto = document.getElementsByClassName("texto")
-  let mostrar = document.getElementsByClassName("mostrar_texto")
+  let texto = document.getElementsByClassName("texto")[0];
+  let mostrar = document.getElementsByClassName("mostrar_texto")[0];
 
   mostrar.innerHTML += texto.value;
 
--- ejercicioPractica06/practica06.js
+++ ejercicioPractica06/practica06.js
@@ -1,8 +1,8 @@
 
-function mayorNum() {
+function compararNum(){
   /* no toma el getElement*/
-  let num1 = parseInt(document.getElementsByClassName("num1").value);
-  let num2 = parseInt(document.getElementsByClassName("num2").value);
+  let num1 = parseInt(document.getElementsByClassName("num1").value)[0];
+  let num2 = parseInt(document.getElementsByClassName("num2").value)[0];
 
   if (num1 && num2) {
     if (num1 >= num2) {
@@ -17,5 +17,4 @@ function mayorNum() {
   } else {
     alert("Uno de los numeros no se relleno")
   }
-
 }
