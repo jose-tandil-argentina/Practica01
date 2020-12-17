77a066de2feae800837081b41e069927ade6aab3
Jose Maria Perez <jm.perez@globant.com>
2020-12-17 13:05:48 -0300

    ejercicios modificados

--- ejercicioPractico08/practico08.js
+++ ejercicioPractico08/practico08.js
@@ -2,14 +2,17 @@
 function insertarNombre() {
   
   
-    let nombre = document.getElementsByClassName("nombre").value;
+    let nombre = document.getElementsByClassName("nombre")[0];
   
     if (nombre == "") {
       alert("El nombre no puede estar vacio");
     } else {
+
+
       let opcion = "<li> " + nombre + "</li>";
   
-      let lista = document.getElementsByClassName("lista-nombres");
+      let lista = document.getElementsByClassName("lista-nombres")[0];
+      
       lista.innerHTML += opcion;
   
       alert("Se ha insertado el nombre");
